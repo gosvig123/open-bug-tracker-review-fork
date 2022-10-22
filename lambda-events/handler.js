599032522e9950feba927efff54f8d24853b4747
eduim <eduardo.im92@gmail.com>
2022-10-22 15:46:24 +0200

    build: basic lambda setup

--- lambda-events/handler.js
+++ lambda-events/handler.js
@@ -1,19 +1,16 @@
 "use strict";
 
 module.exports.reciever = (event, context, callback) => {
-  console.log("event", event);
-  console.log("context", context);
-
-  // const response = {
-  //   statusCode: 200,
-  //   headers: {
-  //     'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-  //   },
-  //   body: JSON.stringify({
-  //     message: 'Go Serverless v1.0! Your function executed successfully!',
-  //     input: event,
-  //   }),
-  // };
+  const response = {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+    },
+    body: JSON.stringify({
+      message: "Response recieved correctly",
+      input: event.body,
+    }),
+  };
 
   callback(null, response);
 };

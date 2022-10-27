e010d319e899165f4bfd3ccb7465ae2f421b81e5
eduim <eduardo.im92@gmail.com>
2022-10-27 11:26:15 +0200

    feat(projects): project detail recieving data to FE

--- client/pages/bugs.tsx
+++ client/pages/bugs.tsx
@@ -1,48 +1,41 @@
-import { Card } from "@contentful/f36-components"
-import { useEffect, useState } from "react"
-import { APIBugs } from "../lib/api"
-
+import { Card } from "@contentful/f36-components";
+import { useEffect, useState } from "react";
+import { APIBugs } from "../lib/api";
 
 interface Bug {
-  id: string
-  message: string
-  solved_at: null
-  first_seen: string
-  last_seen: string
+  id: string;
+  message: string;
+  solved_at: null;
+  first_seen: string;
+  last_seen: string;
   occurrences: [
     {
-      report_date: string,
-      stack_trace: string,
-      meta_data:
-      {
-        user_agent: string,
-        browser: string
-      }
-
+      report_date: string;
+      stack_trace: string;
+      meta_data: {
+        user_agent: string;
+        browser: string;
+      };
     }
-  ]
+  ];
 }
 
-
-
 function BugDetails(): JSX.Element {
-  const [bugDeatails, setbugDetails] = useState<Bug | any>({})
+  const [bugDeatails, setbugDetails] = useState<Bug | any>({});
 
   const getBugDetails = async function () {
-    const result = await APIBugs.bugDetails()
-    console.log(result)
-    setbugDetails(result)
-    console.log(result)
-
-  }
+    const result = await APIBugs.bugDetails();
+    console.log(result);
+    setbugDetails(result);
+    console.log(result);
+  };
 
   useEffect(() => {
-    getBugDetails()
-  }, [])
+    getBugDetails();
+  }, []);
 
   return (
     <div key={bugDeatails.id}>
-
       {/* big part */}
       <Card> solved (not solved){bugDeatails.solved_at}</Card>
       <Card> bug message is {bugDeatails.message}</Card>
@@ -52,26 +45,31 @@ function BugDetails(): JSX.Element {
             <div key={bugDeatails.id}>
               <p> {event.stack_trace}</p>
             </div>
-          )
+          );
         })}
       </div>
 
       {/* info panel */}
-      <p> first seen:  {bugDeatails.first_seen} </p>
+      <p> first seen: {bugDeatails.first_seen} </p>
       <p> last seen: {bugDeatails.last_seen} </p>
       <p> num_occurrences: {bugDeatails.num_occurrences} </p>
 
       {/* chart  */}
-      {bugDeatails.occurrences.map((event: { report_date: string, meta_data: { user_agent: string, browser: string } }) => {
-        return (
-          <div key={bugDeatails.id}>
-            <p> date is {event.report_date} </p>
-            <p> user agent is {event.meta_data.user_agent}</p>
-          </div>
-        )
-      })}
+      {bugDeatails.occurrences.map(
+        (event: {
+          report_date: string;
+          meta_data: { user_agent: string; browser: string };
+        }) => {
+          return (
+            <div key={bugDeatails.id}>
+              <p> date is {event.report_date} </p>
+              <p> user agent is {event.meta_data.user_agent}</p>
+            </div>
+          );
+        }
+      )}
     </div>
-  )
+  );
 }
 
-export default BugDetails
+export default BugDetails;

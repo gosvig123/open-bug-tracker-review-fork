628887b3ae52807f29126eacd46c5fba7b21c81d
bienmag <mvaretckaia@gmail.com>
2022-10-28 19:39:55 +0200

    feat/add one test

--- server/src/controllers/__tests__/projects.controller.test.ts
+++ server/src/controllers/__tests__/projects.controller.test.ts
@@ -1,14 +1,14 @@
 import { describe } from "@jest/globals";
 import { createMockContext } from "@shopify/jest-koa-mocks";
-
 import ProjectController from "../projects.controller";
+
 
 jest.mock("../../models/Projects");
 
 import Project from "../../models/Projects";
 
 function createMockNext() {
-  return () => new Promise(() => {});
+  return () => new Promise(() => { });
 }
 
 describe("createProject", () => {
@@ -28,4 +28,27 @@ describe("createProject", () => {
     // expect(ctx.response.body).toEqual(responseBody);
     // expect(ctx.response.status).toEqual(201);
   });
+
+  test("do not create project without name", async () => {
+    try {
+      const projName = undefined
+      const ctx = createMockContext({ requestBody: { name: projName } })
+      const next = createMockNext()
+
+      const responseBody = { id: 123, name: projName };
+      const spy = jest.spyOn(Project, 'create')
+      // expect(spy).toBeUndefined()
+
+      await ProjectController.createProject(ctx, next)
+      throw new Error('crete did not throw error')
+    } catch (e) {
+      const error = e as Error
+      expect(error.message).toBe('no project name')
+    }
+
+    // expect
+    // expect(projName as any).toBeUndefined()
+    // expect(spy).toHaveBeenCalledTimes(1)
+    // expect(spy).toHaveLength(0)
+  })
 });
